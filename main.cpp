388735fcb40daf7c63b3a03e77e9835dd26b83f1
kikiloop <mkzrong@gmail.com>
2023-07-27 19:28:58 +0800

    add hit

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@
 
 double hit_sphere(const vec3& center, double radius, const ray& r) {
 	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2 * dot(r.direction(), oc);
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
+	auto a = r.direction().length_squared();
+	auto half_b = dot(r.direction(), oc);
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant =half_b * half_b - a * c;
 	if (discriminant < 0) {
 		return -1;
 	}
 	else {
-		return (-b - sqrt(discriminant)) / (2.0 * a);
+		return (-half_b - sqrt(discriminant)) / a;
 	}
 }
 
